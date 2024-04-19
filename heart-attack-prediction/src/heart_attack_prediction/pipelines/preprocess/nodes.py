0aa5133886ddb3eb9f99bc9e138cc6c2dd168c4e
Adam Weber <adam.weber@esky.com>
2024-04-19 18:59:01 +0200

    preprocessed pipeline

---
+++ heart-attack-prediction/src/heart_attack_prediction/pipelines/preprocess/nodes.py
@@ -0,0 +1,10 @@
+
+import pandas
+
+
+def heart_preprocess(heart) -> pandas.CSVDataset:
+    heart.fillna(method='ffill', inplace=True)
+    heart.dropna(inplace=True)
+
+
+    return heart
---
+++ heart-attack-prediction/src/heart_attack_prediction/pipelines/preprocess/pipeline.py
@@ -0,0 +1,19 @@
+"""
+This is a boilerplate pipeline 'preprocess'
+generated using Kedro 0.19.4
+"""
+
+from kedro.pipeline import Pipeline, pipeline
+from .nodes import heart_preprocess
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline(
+        [
+            node(
+                func=heart_preprocess,
+                inputs=['heart'],
+                outputs=['heart_processed'],
+                name="heart_preprocess_node",
+            ),
+        ]
+    )
