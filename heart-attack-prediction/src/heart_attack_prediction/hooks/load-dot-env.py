d439d1bf6dde40984b1c2dc728c68d4748a4f65f
Adamicov <adam.weberit@gmail.com>
2024-04-29 12:00:17 +0200

    update

---
+++ heart-attack-prediction/src/heart_attack_prediction/hooks/load-dot-env.py
@@ -0,0 +1,11 @@
+from kedro.framework.hooks import hook_impl
+from kedro.framework.startup import ProjectHooks
+from dotenv import load_dotenv
+import os
+
+class ProjectHooks(ProjectHooks):
+
+    @hook_impl
+    def before_pipeline_run(self, *args, **kwargs):
+        load_dotenv() 
+
