e681379c351270d58626f36a898571a22db7c322
s7452 <s7452@pjwstk.edu.pl>
2024-06-01 12:31:14 +0200

    Add monitoring

--- backend/heart-attack-prediction/src/heart_attack_prediction/hooks.py
+++ backend/heart-attack-prediction/src/heart_attack_prediction/hooks.py
@@ -1,12 +1,11 @@
 from kedro.framework.hooks import hook_impl
 from kedro.framework.startup import ProjectHooks
 from dotenv import load_dotenv
+from omegaconf import DictConfig
 import os
 
 class ProjectHooks(ProjectHooks):
-
     @hook_impl
     def before_pipeline_run(self, *args, **kwargs):
         print('Running')
-        load_dotenv() 
-
+        load_dotenv()
